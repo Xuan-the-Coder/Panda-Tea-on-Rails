d49d7208d96b5fce644a1477f9a64da70b1ed166
Xuan_the_Coder <wx209dewang@gmail.com>
2020-04-21 16:30:11 -0500

    updated checkout controller and added checkout views

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,14 @@
 class CheckoutController < ApplicationController
+  def create
+    product = Product.find(params[:id])
+
+    if product.nil?
+      redirect_to root_path
+      return
+    end
+
+    respond_to do |format|
+      format.js
+    end
+  end
 end
