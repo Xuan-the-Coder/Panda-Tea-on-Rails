189168f908bdb5346a010fbb4ca7a0921d379efc
Xuan_the_Coder <wx209dewang@gmail.com>
2020-04-22 22:23:17 -0500

    added province abbrs to the province table

---
+++ db/migrate/20200423021915_add_provinces.rb
@@ -0,0 +1,8 @@
+class AddProvinces < ActiveRecord::Migration[6.0]
+  PROVINCES = %w[PE NL NS	NB MB QC ON SK AB BC YT NT NU].freeze
+  def up
+    PROVINCES.each do |province|
+      Province.create(name: province)
+    end
+  end
+end
